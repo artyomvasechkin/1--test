3f5766193e71a106c71435fd6d95186fc0ae66cd
Andrew <looandr02@gmail.com>
2022-03-31 21:55:33 +0300

    minor fixes

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -65,37 +65,55 @@ def solver(a, n, m, restrict=None):
             if a[i][j] == 1:
                 d[j] = i
 
-    while len(st):
-        st.pop()
-
-    for j in range(m):
-        while len(st) > 0 and d[st[len(st) - 1]] <= d[j]:
+        while len(st):
             st.pop()
 
-        d1[j] = -1 if len(st) == 0 else st[len(st) - 1]
-        st.append(j)
+        for j in range(m):
+            while len(st) > 0 and d[st[len(st) - 1]] <= d[j]:
+                st.pop()
 
-    while len(st):
-        st.pop()
+            d1[j] = -1 if len(st) == 0 else st[len(st) - 1]
+            st.append(j)
 
-    for j in range(m - 1, -1, -1):
-        while len(st) and d[st[len(st) - 1]] <= d[j]:
+        while len(st):
             st.pop()
-        d2[j] = m if len(st) == 0 else st[len(st) - 1]
-        st.append(j)
 
-    for j in range(m):
-        area = (i - d[j]) * (d2[j] - d1[j] - 1)
-        if area > ans:
-            A = (d[j] + 1, d1[j] + 1)
-            B = (i, d2[j] - 1)
-            ans = area
+        for j in range(m - 1, -1, -1):
+            while len(st) and d[st[len(st) - 1]] <= d[j]:
+                st.pop()
+            d2[j] = m if len(st) == 0 else st[len(st) - 1]
+            st.append(j)
+
+        for j in range(m):
+            area = (i - d[j]) * (d2[j] - d1[j] - 1)
+            if area > ans:
+                A = (d[j] + 1, d1[j] + 1)
+                B = (i, d2[j] - 1)
+                ans = area
 
     return (ans, A, B)
 
+max_rec = solver(grad, width, height)
+first = True
+rectangles = []
+while max_rec[0] > 1000:
+    if not first:
+        max_rec = solver(grad, width, height)
+    if max_rec[0] < 1000:
+        break
+    first = False
+    for hi in range(max_rec[1][0], max_rec[2][0]):
+        for wi in range(max_rec[1][1], max_rec[2][1]):
+            grad[hi][wi] = 1
+    rectangles.append(max_rec)
+    im = Image.fromarray(image)
+    im.save("your_file.jpeg")
+    for i in range(width):
+        for j in range(height):
+            if grad[i][j] == 0:
+                image[j][i] = [0, 0, 0]
+            else:
+                image[j][i] = [255, 255, 255]
 
-i = solver(grad, width, height)
-for hi in range(i[1][0], i[2][0]):
-    for wi in range(i[1][1], i[2][1]):
-        grad[hi][wi] = 1
+print(rectangles)
 
